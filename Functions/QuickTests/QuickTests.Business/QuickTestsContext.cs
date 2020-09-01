b80003bb31c4613584ad560c2c0b0385d087fbc8
LuYaSC <luisjimpe93@gmail.com>
2020-09-01 11:14:26 -0400

    actualizacion de archivos

---
+++ Functions/QuickTests/QuickTests.Business/QuickTestsContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TC.Core.Data.Context;
+
+namespace TC.Functions.QuickTests.Business
+{
+    public class QuickTestsContext : SBTCContext
+    {
+        public QuickTestsContext(string nameOrConnectionString = "SBTCContext") : base(nameOrConnectionString)
+        {
+        }
+    }
+}
